856af7be7a9ca4476ff277d424dd59459ef46701
ddanglehee <shee9812@naver.com>
2022-04-10 21:12:51 +0900

    ui: Add bottom navigation view to main activity

--- app/src/main/java/com/maru/todayroute/MainActivity.kt
+++ app/src/main/java/com/maru/todayroute/MainActivity.kt
@@ -2,10 +2,28 @@ package com.maru.todayroute
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bnv_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
