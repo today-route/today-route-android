89fd460051d77b975e3f91b2de27e14319aef6eb
ddanglehee <shee9812@naver.com>
2022-04-12 20:52:43 +0900

    refactor: Apply view binding

--- app/src/main/java/com/maru/todayroute/MainActivity.kt
+++ app/src/main/java/com/maru/todayroute/MainActivity.kt
@@ -6,24 +6,23 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.maru.todayroute.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         setBottomNavigation()
     }
 
     private fun setBottomNavigation() {
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bnv_main)
-
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
-        bottomNavigationView.setupWithNavController(navController)
+        binding.bnvMain.setupWithNavController(navController)
     }
 }
