9a66ee1d565ee3c0670b0a6973467e43ec251047
ddanglehee <shee9812@naver.com>
2022-05-06 10:06:21 +0900

    feat: Request location permissions at runtime

--- app/src/main/java/com/maru/todayroute/home/HomeFragment.kt
+++ app/src/main/java/com/maru/todayroute/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.maru.todayroute.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.maru.todayroute.databinding.FragmentHomeBinding
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
@@ -26,6 +30,9 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
         viewLifecycleOwner.lifecycle.addObserver(MapViewLifecycleObserver(binding.mvMap, savedInstanceState))
         binding.mvMap.getMapAsync(this)
+        if (hasNotLocationPermission()) {
+            requestLocationPermission()
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -45,5 +52,39 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(p0: NaverMap) {
 
+    }
+
+    private fun hasNotLocationPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestLocationPermission() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false
+                        || permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false
+                -> {
+                }
+                else -> {
+
+                }
+            }
+        }
+
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 }
