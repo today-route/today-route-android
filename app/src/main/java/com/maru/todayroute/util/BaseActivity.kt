953b47aaa9360b8e82a20ac9ad078565ea0795dc
ddanglehee <shee9812@naver.com>
2022-07-05 22:53:18 +0900

    chore: Add BaseActivity

---
+++ app/src/main/java/com/maru/todayroute/util/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.maru.todayroute.util
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes val layoutResource: Int) :
+    AppCompatActivity() {
+
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResource)
+        binding.lifecycleOwner = this
+    }
+
+}
