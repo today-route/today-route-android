9dd13ae8c181ed47f538f504d328b29bf662b938
ddanglehee <shee9812@naver.com>
2022-07-05 23:57:08 +0900

    feat: Sign in with kakao login api

--- app/src/main/java/com/maru/todayroute/TodayRouteApplication.kt
+++ app/src/main/java/com/maru/todayroute/TodayRouteApplication.kt
@@ -1,8 +1,14 @@
 package com.maru.todayroute
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.maru.todayroute.util.Constants
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class TodayRouteApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, Constants.KAKAO_NATIVE_APP_KEY)
+    }
 }
