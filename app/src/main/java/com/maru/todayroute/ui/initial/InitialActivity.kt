fbde81626ce871ca59425b38a5864cd413903375
ddanglehee <shee9812@naver.com>
2022-08-18 17:32:43 +0900

    chore: Remove unnecessary activity and viewmodel

--- app/src/main/java/com/maru/todayroute/ui/initial/InitialActivity.kt
+++ app/src/main/java/com/maru/todayroute/ui/initial/InitialActivity.kt
@@ -1,8 +1,13 @@
 package com.maru.todayroute.ui.initial
 
+import androidx.activity.viewModels
 import com.maru.todayroute.R
 import com.maru.todayroute.databinding.ActivityInitialBinding
 import com.maru.todayroute.util.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class InitialActivity : BaseActivity<ActivityInitialBinding>(R.layout.activity_initial) {
+
+    private val viewModel: InitialViewModel by viewModels()
 }
--- app/src/main/java/com/maru/todayroute/ui/initial/InitialViewModel.kt
+++ app/src/main/java/com/maru/todayroute/ui/initial/InitialViewModel.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class InitialProfileViewModel @Inject constructor (
+class InitialViewModel @Inject constructor (
     private val userRepository: UserRepository
 ) : ViewModel() {
 
