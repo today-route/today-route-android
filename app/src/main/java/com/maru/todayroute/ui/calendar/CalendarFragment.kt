46234f719b899f3629b6c1f8e900c42315174c3e
dolssun <903ejkim@naver.com>
2022-09-19 18:37:50 +0900

    chore: Add CalendarFragment

---
+++ app/src/main/java/com/maru/todayroute/ui/calendar/CalendarFragment.kt
@@ -0,0 +1,16 @@
+package com.maru.todayroute.ui.calendar
+
+import android.os.Bundle
+import android.view.View
+import com.maru.todayroute.R
+import com.maru.todayroute.databinding.FragmentCalendarBinding
+import com.maru.todayroute.util.BaseFragment
+
+class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment_calendar) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
