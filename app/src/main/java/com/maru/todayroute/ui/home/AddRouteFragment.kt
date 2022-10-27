0e13be188b918292b4764a5faec50e38cee9e769
ddanglehee <shee9812@naver.com>
2022-10-27 21:27:48 +0900

    ui: Add animation to navigation from HomeFragment to AddRouteFragment

--- app/src/main/java/com/maru/todayroute/ui/home/AddRouteFragment.kt
+++ app/src/main/java/com/maru/todayroute/ui/home/AddRouteFragment.kt
@@ -2,6 +2,7 @@ package com.maru.todayroute.ui.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.maru.todayroute.R
 import com.maru.todayroute.databinding.FragmentAddRouteBinding
 import com.maru.todayroute.util.BaseFragment
@@ -12,5 +13,13 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupButtonClickListener()
+    }
+
+    private fun setupButtonClickListener() {
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
