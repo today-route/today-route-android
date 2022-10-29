37fad5af2001a10f7e60172dad2540c02d1b9181
ddanglehee <shee9812@naver.com>
2022-10-29 22:18:40 +0900

    feat: Add location in AddRouteFragment

--- app/src/main/java/com/maru/todayroute/ui/addroute/AddRouteFragment.kt
+++ app/src/main/java/com/maru/todayroute/ui/addroute/AddRouteFragment.kt
@@ -1,6 +1,7 @@
 package com.maru.todayroute.ui.addroute
 
 import android.app.Activity.RESULT_OK
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
@@ -11,8 +12,12 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.MediaStore.Audio.Media
 import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -41,42 +46,61 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
     private val photoListAdapter by lazy { PhotoListAdapter() }
     private val photoList = mutableListOf<Bitmap>()
 
-    private val activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-            if (result.data?.clipData != null) {
-                val data = result.data?.clipData
-                data?.let { clipData ->
-                    val count = data.itemCount
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                if (result.data?.clipData != null) {
+                    val data = result.data?.clipData
+                    data?.let { clipData ->
+                        val count = data.itemCount
 
-                    if (10 < count || 10 < photoList.size + count) {
-                        Toast.makeText(requireContext(), "사진은 최대 10장까지 선택 가능합니다.", Toast.LENGTH_SHORT).show()
-                    } else {
-                        for (i in 0 until clipData.itemCount) {
-                            val uri = clipData.getItemAt(i).uri
-                            if (28 <= Build.VERSION.SDK_INT) {
-                                val source = ImageDecoder.createSource(requireActivity().contentResolver, uri)
-                                photoList.add(ImageDecoder.decodeBitmap(source))
-                            } else {
-                                photoList.add(MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, uri))
+                        if (10 < count || 10 < photoList.size + count) {
+                            Toast.makeText(
+                                requireContext(),
+                                "사진은 최대 10장까지 선택 가능합니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            for (i in 0 until clipData.itemCount) {
+                                val uri = clipData.getItemAt(i).uri
+                                if (28 <= Build.VERSION.SDK_INT) {
+                                    val source = ImageDecoder.createSource(
+                                        requireActivity().contentResolver,
+                                        uri
+                                    )
+                                    photoList.add(ImageDecoder.decodeBitmap(source))
+                                } else {
+                                    photoList.add(
+                                        MediaStore.Images.Media.getBitmap(
+                                            requireActivity().contentResolver,
+                                            uri
+                                        )
+                                    )
+                                }
                             }
                         }
                     }
-                }
-            } else {
-                // 이미지를 한 장만 선택한 경우
-                result.data?.data?.let { uri ->
-                    if (28 <= Build.VERSION.SDK_INT) {
-                        val source = ImageDecoder.createSource(requireActivity().contentResolver, uri)
-                        photoList.add(ImageDecoder.decodeBitmap(source))
-                    } else {
-                        photoList.add(MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, uri))
+                } else {
+                    // 이미지를 한 장만 선택한 경우
+                    result.data?.data?.let { uri ->
+                        if (28 <= Build.VERSION.SDK_INT) {
+                            val source =
+                                ImageDecoder.createSource(requireActivity().contentResolver, uri)
+                            photoList.add(ImageDecoder.decodeBitmap(source))
+                        } else {
+                            photoList.add(
+                                MediaStore.Images.Media.getBitmap(
+                                    requireActivity().contentResolver,
+                                    uri
+                                )
+                            )
+                        }
                     }
                 }
-            }
 
-            photoListAdapter.submitList(ArrayList(photoList))
+                photoListAdapter.submitList(ArrayList(photoList))
+            }
         }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -91,9 +115,13 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
         binding.mvMap.getMapAsync(this)
         setupButtonClickListener()
         setUpObserver()
+        initRecyclerViewAdapter()
+    }
 
+    private fun initRecyclerViewAdapter() {
         binding.rvPhotoList.apply {
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             adapter = photoListAdapter
         }
     }
@@ -103,12 +131,41 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
             findNavController().popBackStack()
         }
 
+        binding.tvLocation.setOnClickListener {
+            showAddLocationDialog()
+        }
+
         binding.btnAddPhotos.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
-            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, MediaStore.Images.Media.CONTENT_TYPE)
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                MediaStore.Images.Media.CONTENT_TYPE
+            )
             intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
             activityResultLauncher.launch(intent)
         }
+    }
+
+    private fun showAddLocationDialog() {
+        val container = FrameLayout(requireContext())
+        val params = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        params.leftMargin = resources.getDimensionPixelSize(R.dimen.dialog_margin)
+        params.rightMargin = resources.getDimensionPixelSize(R.dimen.dialog_margin)
+        params.topMargin = resources.getDimensionPixelSize(R.dimen.dialog_margin)
+
+        val editText = EditText(context)
+        editText.layoutParams = params
+        container.addView(editText)
+
+        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+            .setTitle("위치 추가")
+            .setView(container)
+            .setIcon(R.drawable.ic_baseline_location_on_24)
+            .setPositiveButton("확인") { dialog, _ ->
+                binding.tvLocation.text = editText.text.toString()
+                dialog.dismiss()
+            }
+        alertDialogBuilder.show()
     }
 
     private fun setUpObserver() {
@@ -123,7 +180,7 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
 
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
-        with (viewModel) {
+        with(viewModel) {
             getCenterCoordinate()
             drawRoute()
         }
