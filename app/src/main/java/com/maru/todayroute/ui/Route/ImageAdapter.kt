548bc4882954d5ca4bf210ff0bce9572e8ff4cde
dolssun <903ejkim@naver.com>
2022-10-31 16:59:52 +0900

    feat: Show registered image when you click

--- app/src/main/java/com/maru/todayroute/ui/Route/ImageAdapter.kt
+++ app/src/main/java/com/maru/todayroute/ui/Route/ImageAdapter.kt
@@ -1,23 +1,29 @@
 package com.maru.todayroute.ui.Route
 
-import android.os.Parcel
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.maru.todayroute.R
+import com.maru.todayroute.databinding.FragmentRouteBinding
 
 
-
-class ImageAdapter(val imageList: ArrayList<Images>): RecyclerView.Adapter<ImageAdapter.CustomViewHolder>(){
-
+class ImageAdapter(val imageList: ArrayList<Images>, val binding: FragmentRouteBinding): RecyclerView.Adapter<ImageAdapter.CustomViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_route_image, parent, false)
+
         return CustomViewHolder(view).apply{
+
             itemView.setOnClickListener {
+                val curPos: Int = adapterPosition
+                val image: Images = imageList.get(curPos)
+                println("루트아이디 : ${image.routeId}, 이미지번호 : ${image.id}\n")
+
+                // 선택한 이미지 띄우기
+                binding.ivClickRouteImage.setImageResource(image.url)
 
             }
         }
@@ -26,7 +32,6 @@ class ImageAdapter(val imageList: ArrayList<Images>): RecyclerView.Adapter<Image
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         holder.image.setImageResource(imageList.get(position).url)
-    //holder.title.text = routeList.get(position).title
     }
 
     override fun getItemCount(): Int {
@@ -38,6 +43,5 @@ class ImageAdapter(val imageList: ArrayList<Images>): RecyclerView.Adapter<Image
     class CustomViewHolder(itemview: View): RecyclerView.ViewHolder(itemview) {
         val image = itemview.findViewById<ImageView>(R.id.iv_route_image)
     }
-
 
 }
--- app/src/main/java/com/maru/todayroute/ui/Route/RouteFragment.kt
+++ app/src/main/java/com/maru/todayroute/ui/Route/RouteFragment.kt
@@ -2,6 +2,7 @@ package com.maru.todayroute.ui.Route
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.maru.todayroute.R
@@ -32,14 +33,14 @@ val imageList = arrayListOf<Images>(
     Images(6,R.drawable.hongdae, 0),
     Images(7,R.drawable.linefriends, 0),
     Images(8,R.drawable.lamb,0),
-    Images(9,R.drawable.linefriends,9),
+    Images(9,R.drawable.hongdae1,9),
     Images(10,R.drawable.lamb,9),
     Images(11,R.drawable.linefriends,9),
     Images(12,R.drawable.lamb,9),
-    Images(13,R.drawable.linefriends,9),
+    Images(13,R.drawable.cafe,9),
     Images(14,R.drawable.lamb,9),
     Images(15,R.drawable.linefriends,9),
-    Images(16,R.drawable.lamb,9),
+    Images(16,R.drawable.hongdae1,9),
     Images(17,R.drawable.linefriends,10),
     Images(18,R.drawable.lamb,10),
     Images(19,R.drawable.linefriends,10),
@@ -50,45 +51,45 @@ val imageList = arrayListOf<Images>(
     Images(24,R.drawable.lamb,10),
 )
 
+
 class RouteFragment : BaseFragment<FragmentRouteBinding>(R.layout.fragment_route) {
 
     private val args: RouteFragmentArgs by navArgs()
 
+    fun dateFormat(route: Route) : List<String>{
+
+        val eventDate = route.date.split("-")
+        return eventDate
+    }
+
+    fun showImage(imageList: ArrayList<Images>, routeId: Int){
+        val matchImageList: ArrayList<Images> = ArrayList<Images>()
+
+        for(i in imageList.indices){
+            if(imageList.get(i).routeId == routeId)
+                matchImageList.add(imageList[i])
+        }
+
+
+        binding.rvRouteImage.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvRouteImage.setHasFixedSize(true)
+        binding.rvRouteImage.adapter = ImageAdapter(matchImageList, binding)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        fun dateFormat(route: Route) : List<String>{
-
-            val eventDate = route.date.split("-")
-            return eventDate
-        }
-
-        fun showImage(imageList: ArrayList<Images>, routeId: Int){
-            val matchImageList: ArrayList<Images> = ArrayList<Images>()
-
-            for(i in imageList.indices){
-                if(imageList.get(i).routeId == routeId)
-                    matchImageList.add(imageList[i])
-            }
-
-
-            binding.rvRouteImage.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-            binding.rvRouteImage.setHasFixedSize(true)
-            binding.rvRouteImage.adapter = ImageAdapter(matchImageList)
-        }
 
         binding.apply{
             val route: Route = routeList[args.routeId]
             val dates: List<String> = dateFormat(route)
 
-            binding.tvRouteDateTop.text = "${dates.get(0)}년 ${dates.get(1)}월 ${dates.get(2)}일"
+            binding.tvRouteDateTop.text = "${dates.get(0)}년 ${dates.get(1)}월 ${dates.get(2)}일"  // 루트 작성 날짜
             binding.tvRouteTitle.text = route.title         // 제목
             binding.tvLocation.text = route.location        // 위치
+            binding.ivRoute.visibility = View.VISIBLE
             //binding.ivRoute = route.지도                   // 루트
-            //binding.rvRouteImage = route.사진              // 사진
 
-
-            showImage(imageList, route.id)
+            showImage(imageList, route.id)                  // 등록한 사진 리사이클러뷰 나열
 
             binding.etInsertContent.text = route.content    // 본문내용
         }
