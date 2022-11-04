2c9ab7a26cfce2516cb4652baa3d65075e123977
ddanglehee <shee9812@naver.com>
2022-11-04 17:56:54 +0900

    style: Refactoring AddRouteFragment

--- app/src/main/java/com/maru/todayroute/ui/addroute/AddRouteFragment.kt
+++ app/src/main/java/com/maru/todayroute/ui/addroute/AddRouteFragment.kt
@@ -1,23 +1,16 @@
 package com.maru.todayroute.ui.addroute
 
 import android.app.Activity.RESULT_OK
-import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Color
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.provider.MediaStore.Audio.Media
 import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AlertDialog
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -45,61 +38,27 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
     private lateinit var naverMap: NaverMap
 
     private val photoListAdapter by lazy { PhotoListAdapter() }
-    private val photoList = mutableListOf<Bitmap>()
 
     private val activityResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
+                val photoList = mutableListOf<Bitmap>()
+
                 if (result.data?.clipData != null) {
                     val data = result.data?.clipData
                     data?.let { clipData ->
-                        val count = data.itemCount
-
-                        if (10 < count || 10 < photoList.size + count) {
-                            Toast.makeText(
-                                requireContext(),
-                                "사진은 최대 10장까지 선택 가능합니다.",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        } else {
-                            for (i in 0 until clipData.itemCount) {
-                                val uri = clipData.getItemAt(i).uri
-                                if (28 <= Build.VERSION.SDK_INT) {
-                                    val source = ImageDecoder.createSource(
-                                        requireActivity().contentResolver,
-                                        uri
-                                    )
-                                    photoList.add(ImageDecoder.decodeBitmap(source))
-                                } else {
-                                    photoList.add(
-                                        MediaStore.Images.Media.getBitmap(
-                                            requireActivity().contentResolver,
-                                            uri
-                                        )
-                                    )
-                                }
-                            }
+                        for (i in 0 until clipData.itemCount) {
+                            val uri = clipData.getItemAt(i).uri
+                            photoList.add(getBitmapFromUri(uri))
                         }
                     }
                 } else {
                     // 이미지를 한 장만 선택한 경우
                     result.data?.data?.let { uri ->
-                        if (28 <= Build.VERSION.SDK_INT) {
-                            val source =
-                                ImageDecoder.createSource(requireActivity().contentResolver, uri)
-                            photoList.add(ImageDecoder.decodeBitmap(source))
-                        } else {
-                            photoList.add(
-                                MediaStore.Images.Media.getBitmap(
-                                    requireActivity().contentResolver,
-                                    uri
-                                )
-                            )
-                        }
+                        photoList.add(getBitmapFromUri(uri))
                     }
                 }
-
-                photoListAdapter.submitList(ArrayList(photoList))
+                viewModel.addPhotos(photoList)
             }
         }
 
@@ -118,6 +77,19 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
         setUpObserver()
         addTextChangedListener()
         initRecyclerViewAdapter()
+    }
+
+    private fun getBitmapFromUri(uri: Uri): Bitmap {
+        return if (28 <= Build.VERSION.SDK_INT) {
+            val source =
+                ImageDecoder.createSource(requireActivity().contentResolver, uri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            MediaStore.Images.Media.getBitmap(
+                requireActivity().contentResolver,
+                uri
+            )
+        }
     }
 
     private fun initRecyclerViewAdapter() {
@@ -153,10 +125,6 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
             findNavController().popBackStack()
         }
 
-//        binding.tvLocation.setOnClickListener {
-//            showAddLocationDialog()
-//        }
-
         binding.btnAddPhotos.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
             intent.setDataAndType(
@@ -168,35 +136,24 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
         }
     }
 
-//    private fun showAddLocationDialog() {
-//        val container = FrameLayout(requireContext())
-//        val params = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-//        params.leftMargin = resources.getDimensionPixelSize(R.dimen.dialog_margin)
-//        params.rightMargin = resources.getDimensionPixelSize(R.dimen.dialog_margin)
-//        params.topMargin = resources.getDimensionPixelSize(R.dimen.dialog_margin)
-//
-//        val editText = EditText(context)
-//        editText.layoutParams = params
-//        container.addView(editText)
-//
-//        val alertDialogBuilder = AlertDialog.Builder(requireContext())
-//            .setTitle("위치 추가")
-//            .setView(container)
-//            .setIcon(R.drawable.ic_baseline_location_on_24)
-//            .setPositiveButton("확인") { dialog, _ ->
-//                binding.tvLocation.text = editText.text.toString()
-//                dialog.dismiss()
-//            }
-//        alertDialogBuilder.show()
-//    }
-
     private fun setUpObserver() {
-        viewModel.drawRoute.observe(viewLifecycleOwner) { geoCoordList ->
-            drawRoute(geoCoordList)
-        }
-
-        viewModel.centerCoord.observe(viewLifecycleOwner) { center ->
-            moveCameraToCenterCoordinate(center)
+        with(viewModel) {
+            drawRoute.observe(viewLifecycleOwner) { geoCoordList ->
+                drawRoute(geoCoordList)
+            }
+            centerCoord.observe(viewLifecycleOwner) { center ->
+                moveCameraToCenterCoordinate(center)
+            }
+            photoBitmapList.observe(viewLifecycleOwner) { photoBitmapList ->
+                photoListAdapter.submitList(photoBitmapList)
+            }
+            showToastMessage.observe(viewLifecycleOwner) {
+                Toast.makeText(
+                    requireContext(),
+                    "사진은 최대 10장까지 선택 가능합니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
@@ -211,7 +168,7 @@ class AddRouteFragment : BaseFragment<FragmentAddRouteBinding>(R.layout.fragment
 
     private fun drawRoute(geoCoordList: List<LatLng>) {
         val path = PathOverlay()
-        with (path) {
+        with(path) {
             color = requireContext().getColor(R.color.purple)
             outlineColor = requireContext().getColor(R.color.purple)
             width = 30
--- app/src/main/java/com/maru/todayroute/ui/addroute/AddRouteViewModel.kt
+++ app/src/main/java/com/maru/todayroute/ui/addroute/AddRouteViewModel.kt
@@ -1,9 +1,11 @@
 package com.maru.todayroute.ui.addroute
 
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.maru.todayroute.util.RouteUtils
+import com.maru.todayroute.util.SingleLiveEvent
 import com.naver.maps.geometry.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -17,6 +19,10 @@ class AddRouteViewModel @Inject constructor() : ViewModel() {
     private val _drawRoute: MutableLiveData<List<LatLng>> = MutableLiveData()
     val centerCoord: LiveData<LatLng> get() = _centerCoord
     private val _centerCoord: MutableLiveData<LatLng> = MutableLiveData()
+    val photoBitmapList: LiveData<List<Bitmap>> get() = _photoBitmapList
+    private val _photoBitmapList: MutableLiveData<List<Bitmap>> = MutableLiveData(listOf())
+    val showToastMessage: LiveData<Unit> get() = _showToastMessage
+    private val _showToastMessage: SingleLiveEvent<Unit> = SingleLiveEvent()
 
     fun setGeoCoordList(geoCoordList: List<LatLng>) {
         this.geoCoordList = geoCoordList
@@ -31,5 +37,19 @@ class AddRouteViewModel @Inject constructor() : ViewModel() {
         val longitudeList = geoCoordList.map { it.longitude }
 
         _centerCoord.value = RouteUtils.calculateCenterCoordinate(latitudeList, longitudeList)
+    }
+
+    fun addPhotos(newPhotoList: List<Bitmap>) {
+        val photoList = mutableListOf<Bitmap>()
+        val oldPhotoList = _photoBitmapList.value!!
+
+        if (10 < oldPhotoList.size + newPhotoList.size) {
+            _showToastMessage.call()
+        } else {
+            photoList.addAll(_photoBitmapList.value!!)
+            photoList.addAll(newPhotoList)
+
+            _photoBitmapList.value = photoList
+        }
     }
 }
