2106139f41bbeb1da73e1e494e386045bfe1ca1f
ddanglehee <shee9812@naver.com>
2022-11-14 23:57:42 +0900

    feat: Sign out

--- app/src/main/java/com/maru/todayroute/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/maru/todayroute/ui/settings/SettingsFragment.kt
@@ -1,13 +1,19 @@
 package com.maru.todayroute.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.maru.todayroute.R
 import com.maru.todayroute.databinding.FragmentSettingsBinding
+import com.maru.todayroute.ui.initial.InitialActivity
 import com.maru.todayroute.util.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
 
     private val viewModel: SettingsViewModel by viewModels()
@@ -15,13 +21,48 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupButtonClickListener()
+        setupObserver()
     }
 
     private fun setupButtonClickListener() {
-        with (binding) {
+        with(binding) {
             btnBack.setOnClickListener {
                 findNavController().popBackStack()
             }
+
+            btnSignOut.setOnClickListener {
+                showAlertDialog()
+            }
         }
+    }
+
+    private fun showAlertDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(requireActivity())
+            .setTitle("로그아웃")
+            .setMessage("정말로 로그아웃 하시겠어요?")
+            .setPositiveButton("로그아웃") { _, _ ->
+                viewModel.signOut()
+            }
+            .setNegativeButton("아니요") { dialog, _ ->
+                dialog.dismiss()
+            }
+        alertDialogBuilder.show()
+    }
+
+    private fun setupObserver() {
+        with (viewModel) {
+            moveToInitialActivity.observe(viewLifecycleOwner) {
+                this@SettingsFragment.moveToInitialActivity()
+                requireActivity().finish()
+            }
+            showToastMessage.observe(viewLifecycleOwner) { message ->
+                Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun moveToInitialActivity() {
+        val intent = Intent(requireContext(), InitialActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/maru/todayroute/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/maru/todayroute/ui/settings/SettingsViewModel.kt
@@ -1,9 +1,37 @@
 package com.maru.todayroute.ui.settings
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kakao.sdk.user.UserApiClient
+import com.maru.data.repository.TokenRepository
+import com.maru.todayroute.util.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() : ViewModel() {
+class SettingsViewModel @Inject constructor(
+    private val tokenRepository: TokenRepository
+) : ViewModel() {
+
+    val moveToInitialActivity: LiveData<Unit> get() = _moveToInitialActivity
+    private val _moveToInitialActivity: SingleLiveEvent<Unit> = SingleLiveEvent()
+    val showToastMessage: LiveData<String> get() = _showToastMessage
+    private val _showToastMessage: MutableLiveData<String> = MutableLiveData()
+
+    fun signOut() {
+        UserApiClient.instance.logout { _ ->
+            viewModelScope.launch(Dispatchers.IO) {
+                tokenRepository.removeTokens()
+                withContext(Dispatchers.Main) {
+                    _showToastMessage.postValue("로그아웃 되었습니다.")
+                    _moveToInitialActivity.call()
+                }
+            }
+        }
+    }
 }
