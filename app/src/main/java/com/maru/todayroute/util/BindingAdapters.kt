2c163230c32c9ce5669e3e9dfda444dbb31f1dae
ddanglehee <shee9812@naver.com>
2023-03-02 22:49:55 +0900

    feat: Add placeholder on couple profile image

--- app/src/main/java/com/maru/todayroute/util/BindingAdapters.kt
+++ app/src/main/java/com/maru/todayroute/util/BindingAdapters.kt
@@ -4,12 +4,14 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
 import com.google.android.material.imageview.ShapeableImageView
+import com.maru.todayroute.R
 
 @BindingAdapter("app:imageUrl")
 fun bindProfileImage(imageView: ShapeableImageView, imageUrl: String?) {
     if (imageUrl != null) {
         Glide.with(imageView)
             .load(imageUrl)
+            .placeholder(R.drawable.ic_circle_user_profile)
             .override(Target.SIZE_ORIGINAL)
             .into(imageView)
         imageView.clipToOutline = true
